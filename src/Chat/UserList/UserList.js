159cf8867b88ca11a02f0799d1f34cc20c172df2
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-14 19:53:29 +0100

    Revert "user list ui"
    This reverts commit 63a5103f3da801391bf6c7a038f248202300707c.

--- src/Chat/UserList/UserList.js
+++ src/Chat/UserList/UserList.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const UserList = ({ user, users }) => {
-  const userList = users.filter(u => u.id !== user.id)
-    .map(u => (
-      <div className="user-list-item" key={u.id}>
-        <div className="user-name">
-          { u.nick }
-        </div>
-      </div>
-    ));
-  return (
-    <div className="user-list">
-      <div className="user-list-item" key={user.id}>
-        <div className="user-name">
-          { user.nick } (You)
-        </div>
-      </div>
-      <p>Online Users</p>
-      { userList }
-    </div>
-  );
-};
-
-
-UserList.propTypes = {
-  user: PropTypes.object.isRequired,
-  users: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+const UserList = () => (
+  <div>
+    User List
+  </div>
+);
 
 export default UserList;
--- src/Chat/UserList/index.js
+++ src/Chat/UserList/index.js
@@ -1,9 +1,3 @@
-import { connect } from 'react-redux';
 import UserList from './UserList';
 
-const mapStateToProps = ({ app }) => ({
-  user: app.user,
-  users: app.users,
-});
-
-export default connect(mapStateToProps, null)(UserList);
+export default UserList;
