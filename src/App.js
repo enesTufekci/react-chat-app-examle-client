5919d0b23805d8f3fe29a9a37c4a885efd781da5
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-14 16:24:33 +0100

    redux integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <div className="App">
-    Chat App
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      Chat App
+    </div>
+  </Provider>
+
 );
 
 export default App;
