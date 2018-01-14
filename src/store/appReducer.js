7fddc458c6fd53f7c2895de5d625bc175e226bc6
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-14 19:37:32 +0100

    app initial state with user and users

--- src/store/appReducer.js
+++ src/store/appReducer.js
@@ -1,8 +1,17 @@
+import { internet } from 'faker';
+import { v4 } from 'uuid';
+
 const ACTION_HANDLERS = {
   //
 };
 
-const initialState = {};
+const initialState = {
+  users: [],
+  user: {
+    nick: internet.username(),
+    id: v4(),
+  },
+};
 
 const appReducer = (state = initialState, action) => {
   const handler = ACTION_HANDLERS[action.type];
