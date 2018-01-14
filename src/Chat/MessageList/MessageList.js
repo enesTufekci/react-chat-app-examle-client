81799e9ed50a4b119c1a72648812f5593910636d
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-14 22:46:47 +0100

    message list view

--- src/Chat/MessageList/MessageList.js
+++ src/Chat/MessageList/MessageList.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const MessageList = () => (
-  <div>
-    Message List
-  </div>
-);
+class MessageList extends Component {
+  render() {
+    const { messages, user } = this.props;
+    let messageList = [];
+    let lastNick;
+    messageList = messages.map((message) => {
+      const { nick, id } = message.user;
+      const direction = id !== user.id ? ' left' : '';
+      let title = '';
+      if (nick !== lastNick) {
+        title = (
+          <div key={message.date} className={`message-title${direction}`}>
+            { id === user.id ? 'You' : nick }
+          </div>
+        );
+      }
+      lastNick = nick;
+
+      return [
+        title,
+        <div
+          className={`message ${message.type} ${direction}`}
+          key={message.uuid}
+          dangerouslySetInnerHTML={{ __html: message.text }}
+        />,
+      ];
+    });
+    return (
+      <div className="message-list">
+        { messageList }
+      </div>
+    );
+  }
+}
+
+MessageList.defaultProps = {
+  user: {},
+  messages: [],
+};
+
+MessageList.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.object),
+  user: PropTypes.object,
+};
 
 export default MessageList;
