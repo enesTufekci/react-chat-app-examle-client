f17c274d9db0e4572d1771b7c8db46525332b9cb
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 02:51:12 +0100

    new action handlers: INPUT_UPDATED & USER_MESSAGE_SENT

--- src/Chat/reducer.js
+++ src/Chat/reducer.js
@@ -1,8 +1,17 @@
+import {
+  INPUT_UPDATED,
+  USER_SENT_MESSAGE,
+} from 'common/actionTypes';
+
 export const ACTION_HANDLERS = {
-  //
+  [INPUT_UPDATED]: (state, action) =>
+    ({ ...state, input: action.payload }),
+  [USER_SENT_MESSAGE]: (state, action) =>
+    ({ ...state, input: '', messages: [...state.messages, action.payload] }),
 };
 
 const initialState = {
+  input: '',
   messages: [],
 };
 
