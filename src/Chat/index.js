8d29070ae9631d37c568761f896709cafb5ef622
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 04:51:56 +0100

    Chat component redux connection

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,10 +1,16 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import UserList from './UserList';
 import MessageList from './MessageList';
 import MessageInput from './MessageInput';
+import { connectUser } from './reducer';
 
 class Chat extends Component {
+  componentDidMount() {
+    this.props.connectUser();
+  }
+
   render() {
     return (
       <div>
@@ -18,4 +24,8 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+Chat.propTypes = {
+  connectUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, { connectUser })(Chat);
