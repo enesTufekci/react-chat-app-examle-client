8cba3f8264a89429447197ae5bf25b280561592a
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 18:09:06 +0100

    epics control typing signals

--- src/store/rootEpic.js
+++ src/store/rootEpic.js
@@ -1,1 +1,30 @@
-export default {};
+import { combineEpics } from 'redux-observable';
+import { Observable } from 'rxjs';
+
+export const isTypingThrottleEpic = action$ =>
+  action$.ofType('INPUT_UPDATED')
+    .throttleTime(1500)
+    .mapTo({ type: 'IS_TYPING_BRIDGE' });
+
+export const isTypingBridgeEpic = action$ =>
+  action$.ofType('IS_TYPING_BRIDGE')
+    .mapTo({ type: 'IS_TYPING', meta: { socket: true } });
+
+export const typingStoppedEpic = action$ =>
+  action$.ofType('IS_TYPING')
+    .mergeMap(() =>
+      Observable.timer(2000)
+        .takeUntil(action$.ofType('IS_TYPING_BRIDGE'))
+        .mapTo({ type: 'TYPING_STOPPED', meta: { socket: true } }));
+
+export const messageSentEpic = action$ =>
+  action$.ofType('USER_SENT_MESSAGE')
+    .mapTo({ type: 'TYPING_STOPPED', meta: { socket: true } });
+
+
+export default combineEpics(
+  isTypingThrottleEpic,
+  isTypingBridgeEpic,
+  messageSentEpic,
+  typingStoppedEpic,
+);
