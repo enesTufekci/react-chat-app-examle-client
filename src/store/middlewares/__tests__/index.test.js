bb486028a02fabc5dbf741b0b86d9a88ed26bb2b
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 04:09:05 +0100

    uuid middleware test

--- src/store/middlewares/__tests__/index.test.js
+++ src/store/middlewares/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import { userMiddleware } from '../index';
+import { userMiddleware, uuidMiddleware } from '../index';
 
 describe('#userMiddleware', () => {
   const user = { nick: 'enes' };
@@ -18,6 +18,28 @@ describe('#userMiddleware', () => {
     const { next, invoke } = create();
     const action = { type: 'TEST', meta: { user: true } };
     const expectedAction = { ...action, payload: { user } };
+    invoke(action);
+    expect(next).toHaveBeenCalledWith(expectedAction);
+  });
+});
+
+describe('#uuidMiddleware', () => {
+  const uuid = 'uuid';
+  const v4 = () => uuid;
+  const create = () => {
+    const store = {
+      getState: jest.fn(() => ({})),
+      dispatch: jest.fn(),
+    };
+    const next = jest.fn();
+    const invoke = action => uuidMiddleware(v4)(store)(next)(action);
+
+    return { store, next, invoke };
+  };
+  it('should attach uuid to action payload', () => {
+    const { next, invoke } = create();
+    const action = { type: 'TEST', meta: { uuid: true } };
+    const expectedAction = { ...action, payload: { uuid } };
     invoke(action);
     expect(next).toHaveBeenCalledWith(expectedAction);
   });
