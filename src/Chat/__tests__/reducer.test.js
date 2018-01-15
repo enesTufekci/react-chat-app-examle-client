614eea77f1ff88bccf5da1f981da62f2ed4aa2c9
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 05:22:52 +0100

    chat reducer tests

--- src/Chat/__tests__/reducer.test.js
+++ src/Chat/__tests__/reducer.test.js
@@ -1,8 +1,46 @@
 import {
   USER_CONNECTED,
+  INPUT_UPDATED,
+  USER_SENT_MESSAGE,
 } from 'common/actionTypes';
 
-import { connectUser } from '../reducer';
+import chatReducer, { connectUser } from '../reducer';
+
+const initialState = {
+  input: '',
+  messages: [],
+};
+
+describe('#chatReducer', () => {
+  it('should return initial state', () => {
+    expect(chatReducer(undefined, {})).toEqual(initialState);
+  });
+  it('should handle INPUT_UPDATED', () => {
+    const input = 'input';
+    const action = {
+      type: INPUT_UPDATED,
+      payload: input,
+    };
+    const expectedState = {
+      ...initialState,
+      input,
+    };
+    expect(chatReducer(initialState, action)).toEqual(expectedState);
+  });
+
+  it('should handle USER_SENT_MESSAGE', () => {
+    const message = 'message';
+    const action = {
+      type: USER_SENT_MESSAGE,
+      payload: message,
+    };
+    const expectedState = {
+      ...initialState,
+      messages: [message],
+    };
+    expect(chatReducer(initialState, action)).toEqual(expectedState);
+  });
+});
 
 describe('#connectUser', () => {
   it('should create connect user action', () => {
