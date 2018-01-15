e699422163fafd277d6e258031d5889289c00a83
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-01-15 18:02:58 +0100

    rxjs & redux-observable integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 /* eslint-disable import/no-extraneous-dependencies */
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
 import throttle from 'lodash/throttle';
 import { v4 } from 'uuid';
 import moment from 'moment';
+
+import rootEpic from './rootEpic';
 import appReducer from './appReducer';
 import chatReducer from '../Chat/reducer';
-
 import { loadState, saveState } from './localStorage';
 import { userMiddleware, dateMiddleware, uuidMiddleware } from './middlewares';
 import socketMiddleware from './middlewares/socketMiddleware';
@@ -15,6 +17,7 @@ const configureStore = () => {
   let enhancers = [];
   const persistedState = { ...loadState('state') };
   const middlewares = [
+    createEpicMiddleware(rootEpic),
     userMiddleware,
     uuidMiddleware(v4),
     dateMiddleware(() => moment().format('DD.MM.YYYY, h:mm:ss a')),
